522f5f2e842cbe9751d6b92f43b92befa00c66ac
Rahul <33891922+SKRahulSK@users.noreply.github.com>
2018-06-11 10:45:34 +0200

    Using only one window
    Starting the command in the first window

--- TmuxOnBoot/start_tmux.sh
+++ TmuxOnBoot/start_tmux.sh
@@ -11,17 +11,11 @@ logfile=$Pr_dir'/tmuxstart.log'
 
 #cd /home/lamp/meschup-client-rpi >> /home/pi/RPI-Scripts-master/startleapd.log 2>&1
 
-/usr/bin/tmux new-session -d -s sess1 >> $logfile 2>&1
+/usr/bin/tmux new-session -d -s sess1 "sudo ${Pr_dir}/trial.sh >> $logfile" >> $logfile 2>&1
 
 /usr/bin/tmux set-option -t sess1 set-remain-on-exit on
 
-/usr/bin/tmux new-window -d -n 'trial' -t sess1:1 "sudo ${Pr_dir}/trial.sh >> $logfile"
-
-#/usr/bin/tmux a -t sess1 "sudo ${Pr_dir}/trial.sh >> $logfile"
-
-#tmux kill-session -t sessionname
-
-#nohup node index.js >/home/lamp/meschup-client-rpi/startup.log >> /home/pi/RPI-Scripts-master/startleapd.log 2>&1
+#/usr/bin/tmux new-window -d -n 'trial' -t sess1:1 "sudo ${Pr_dir}/trial.sh >> $logfile"
 
 
 
